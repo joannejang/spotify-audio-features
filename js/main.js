6b07fa9b8a620f33fe0525e1e4f9b6ccf14f08b0
Zheng Wu <wuz1990@gmail.com>
2016-06-05 00:25:44 -0700

    some stuff

--- js/main.js
+++ js/main.js
@@ -179,17 +179,20 @@ var two_tracks = track_one + track_two;
         console.log(track.id);
         currentApi.getAudioFeatures(track.id).then(function (data) {
             if (one) {
-                console.log("ONE");
+                console.log("I GOT CALLED FOR BEYONCE");
                 //track_one = track;
                 for (var key in data) track_one[key]=data[key];
-                dataset[0] = track_one;
+                //dataset[0] = track_one;
                 console.log("AFTER UPDATING DATASET's one: ");
                 console.log(dataset.length);
                 console.log(dataset);
-                var object_to_add = {"name": track.name, "popularity": track.popularity, "preview_url": track.preview_url, "artist": track.artists[0], "album_title": track.album.name, "image": getSuitableImage(track.album.images),"danceability": data["danceability"], "valence": data["valence"], "tempo": data["tempo"], "energy": data["energy"]};
+                var object_to_add = {"id": track.id, "name": track.name, "popularity": track.popularity, "preview_url": track.preview_url, "artist": track.artists[0].name, "album_title": track.album.name, "image": getSuitableImage(track.album.images),"danceability": data["danceability"], "valence": data["valence"], "tempo": data["tempo"], "energy": data["energy"], "loudness": data["loudness"], "speechiness": data["speechiness"], "acousticness": data["acousticness"], "instrumentalness": data["instrumentalness"], "liveness": data["liveness"] };
+                console.log("BELOW IS OBJECT_TO_ADD");
                 console.log(object_to_add);
                 dataset[dataset.length] = object_to_add;
                 d3.select("svg").remove();
+                console.log("BELOW IS THE ENTIRE DATASET");
+                console.log(dataset);
                 showScatterPlot(dataset, curr_feature);
                 //update_scatter(object_to_add);
                 update_track(dataset, true);
@@ -215,11 +218,11 @@ var two_tracks = track_one + track_two;
     function update_track(track_obj, one) {
         $('#progress1').empty();
         if (one) {
-            var value = '<div style="width: 400px; height: 400px;"><img src="' + getSuitableImage(track_obj[0].album.images) + '"/></div>';
+            var value = '<div style="width: 400px; height: 400px;"><img src="' + track_obj[track_obj.length-1].image + '"/></div>';
             document.getElementById('track-one').innerHTML = value;   
-            document.getElementById('track-title').innerHTML = 'Title: <b>' + track_obj[0].name + '</b>';
-            document.getElementById('track-artist').innerHTML = 'Artist: <b>' + track_obj[0].artists[0].name + '</b>';         
-            document.getElementById('track-album').innerHTML = 'Album:  <b><i>' + track_obj[0].album.name + '</i></b>';
+            document.getElementById('track-title').innerHTML = 'Title: <b>' + track_obj[track_obj.length-1].name + '</b>';
+            document.getElementById('track-artist').innerHTML = 'Artist: <b>' + track_obj[track_obj.length-1].artist + '</b>';         
+            document.getElementById('track-album').innerHTML = 'Album:  <b><i>' + track_obj[track_obj.length-1].album_title + '</i></b>';
             document.getElementById('track-audio-features').innerHTML = '<b>Features</b><ul>' + '<li>Danceability: ' + dataset[dataset.length-1].danceability + '</li>' + '<li>Valence: ' + dataset[dataset.length-1].valence + '</li>' + '<li>Tempo: ' + dataset[dataset.length-1].tempo + '</li>' + '<li>Energy: ' + dataset[dataset.length-1].energy + '</li>' +'</ul>';
         } else {
             var value = '<div style="width: 400px; height: 400px;"><img src="' + getSuitableImage(track_obj[1].album.images) + '"/></div>';
